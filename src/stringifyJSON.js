d62a5d6647ac7131380c1c41dc48231a4573392a
ChadFong <ChadMFong@Gmail.com>
2015-01-20 14:05:50 -0800

    Created stringifyArray to replace stringWriter. stringifyArray takes an array and converts it to a string, accounting for nested and empty arrays.  While stringWriter only worked on shallow arrays.

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,53 +3,124 @@
 
 // but you don't so you're going to write it from scratch:
 
+var cat = [[[0,1],2],[3,4]];
+/*
 var stringifyJSON = function(obj) {
-  if(Array.isArray(obj)){
-  	writer('[', obj, ']');
+  var stringified = '';
+
+
+  var writer = function(workingOn, toDo) {
+
+	if(Array.isArray(workingOn)){
+		if(!nestedTest(workingOn) )
+  		stringified += arrayWriter(workingOn);
+  	}
+
+  	else if(typeof workingOn === 'object') {
+  		stringified += objectWriter(workingOn);
+  	}
+
+  	else if()
+
+
+  
+  if(Array.isArray(obj)) {
+  	if(!Array.isArray(obj[0])) {
+  		return arrayWriter(obj) + stringifyJSON(obj.slice();
+  	}
+
+
+  		return stringifyJSON(obj[0]);
+  	}
+  	else
+
+  	return arrayWriter(obj);
   }
   else if(typeof obj === 'object') {
-  	writer('{', obj, '}');
+  	objectWriter(obj);
   }
   else if(typeof obj === 'string') {
-  	writer('\'', obj, '\'');
+  	writer('\""', obj, '\"');
   }
   else {
-  	writer('',obj);
+  	//writer('',obj);
   }
 };
 
-var writer = function (written, toWrite, end) {
+var basicElements = function(input) {
+	if()
 
-// Needs a base-case
-
-	if(Array.isArray(toWrite)){
-		written += arrayWriter(toWrite);
-		toWrite = end.splice(0,1);
- 	 	writer(written, toWrite, end); // Incorrect implementation — filler.  end will go to null
-	}
-	else if(typeof toWrite === 'object') {
-		writer('{', toWrite, '}');
-	}
-	else if(typeof toWrite === 'string') {
-	  	writer('\'', toWrite, '\'');
-	}
-	else {
-	  	writer('',toWrite);
-	  }
 };
 
-var arrayWriter = function(array) {
-	var first = stringTester(array[0]);
-	var result = '[' + first;
-	for(var i=1 ; i<array.length ; i++) {
-		if(typeof array[i] === 'string'){
-			result += ', ' +"\"" + array[i] + "\"";
-		}
-		else{
-		result += ', ' + array[i];
-	}
-	}
-	return result += ']';
+var 
+*/
+var nestedTest = function(potentiallyNested) {
+
+	var isObject = function(testee) {
+		return function() {
+			return typeof testee === 'object';
+		};
+	};
+
+	return _.some(potentiallyNested, Array.isArray) || _.some(potentiallyNested, isObject);
+};
+
+stringifyArray = function(nestedArray, result) {
+
+  var nestedChecker = function(potentiallyNestedArray) {
+  return _.some(potentiallyNestedArray, Array.isArray);
+};
+    if(result === undefined) {
+      result = '[';
+    }
+    var stringifier = function(current, rest) {
+      if((current === undefined || current.length === 0) && (rest.length === 0)) {
+        return result;
+      }
+
+      else if(current[0] === undefined) {                 //  Clause to catch arrays wrapped in arrays
+        result += ']';
+        stringifier(rest.shift(), rest);
+      }
+
+      else if(Array.isArray(current[0])) {       // If first element is an array
+
+        if(result.slice(-2) !== ', ' && result.slice(-1) !== '[') {          //CURRENT WORKING AREA
+          result += ', ';
+        }
+
+        result += '[';
+        rest.unshift(current.slice(1));          // Put slice everything else onto REST, recur off of first element
+        stringifier(current[0], rest);                // Does not write to result.
+      }
+      else if (!nestedChecker(current)) {        //  If first element is not an array and there are no arrays in current
+        if(result.slice(-2) !== ', ' && result.slice(-1) !== '[') {
+          result += ', ' + current[0];
+        }
+        else {
+          result += current[0];
+        }
+        for(var i=1 ; i<current.length ; i++) {  //  Iterate through and copy over
+          result += ', ' + current[i];                  //  Writes to result
+        }
+        result += ']';
+        stringifier(rest.shift(), rest);                //  Recurs
+      }
+      else {                                     // Otherwise later elements in current are arrays, just take first element.
+        result += current[0] + ', ';
+        rest.unshift(current.slice(1));
+        stringifier(rest.shift(), rest);
+      }
+    };
+    stringifier(nestedArray, []);
+    var openParen = result.split(/[\[]/g).length-1;
+    var closeParen = result.split(/[\]]/g).length-1;
+    var parens = openParen - closeParen;
+    while(parens >0) {
+      parens--;
+      result+= ']';
+    }
+    return result;
 };
 
 var objectWriter = function(obj) {
