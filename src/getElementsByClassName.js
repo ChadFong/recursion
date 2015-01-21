e5d802b46cfd69aec533be973bd0021c62d0e328
ChadFong <ChadMFong@Gmail.com>
2015-01-21 02:03:09 -0800

    Added functions: textNodeRemover — Takes an array of nodes including #text nodes.  Returns an array without the #text nodes.
    classMatcher — Takes a node and the desired class(es), returns true if node contains all classes listed are in the node.

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,38 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-){
-  // your code here
+/*
+var getElementsByClassName = function(className){
+	var desiredElements = [], remainingNodes = [];
+	if(document.body.className === className) {
+		desiredElements.push(document.body);
+	}
+	var nodeFinder = function(aNode) {
+		var children = textNodeRemover(aNode.childNodes);
+		remainingNodes.unshift(children.slice(1));
+		if(children[0].classList)
+
+	}
+
+};
+*/
+var classMatcher = function(node, desiredClass) {
+	var testResult = true;
+	var classArray = desiredClass.split(/[\ ]/g);
+	for(var i=0 ; i<classArray.length ; i++) {
+		if(!node.classList.contains(classArray[i])) {
+			testResult = false;
+		}
+	}
+	return testResult;
+};
+
+var textNodeRemover = function(array) {
+	var textNodes = [];
+	_.each(array, function(aNode) {
+		if(aNode.nodeType !== 3) {
+			textNodes.push(aNode);
+		}
+	});
+	return textNodes;
 };
